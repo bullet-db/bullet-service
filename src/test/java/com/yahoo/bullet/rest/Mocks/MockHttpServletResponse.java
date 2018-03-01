b424b54ac4a097c8276fa42da7093a3f1e5894f3
Nathan Speidel <nspeidel@yahoo-inc.com>
2018-03-01 12:48:21 -0800

    Added tests

---
+++ src/test/java/com/yahoo/bullet/rest/Mocks/MockHttpServletResponse.java
@@ -0,0 +1,203 @@
+/*
+ *  Copyright 2016, Yahoo Inc.
+ *  Licensed under the terms of the Apache License, Version 2.0.
+ *  See the LICENSE file associated with the project for terms.
+ */
+package com.yahoo.bullet.rest.Mocks;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Locale;
+
+public class MockHttpServletResponse implements HttpServletResponse {
+    int status = 200;
+
+    @Override
+    public void setStatus(int i) {
+        this.status = i;
+    }
+
+    @Override
+    public void setStatus(int i, String s) {
+        this.status = i;
+    }
+
+    @Override
+    public int getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public void addCookie(Cookie cookie) {
+
+    }
+
+    @Override
+    public boolean containsHeader(String s) {
+        return false;
+    }
+
+    @Override
+    public String encodeURL(String s) {
+        return null;
+    }
+
+    @Override
+    public String encodeRedirectURL(String s) {
+        return null;
+    }
+
+    @Override
+    public String encodeUrl(String s) {
+        return null;
+    }
+
+    @Override
+    public String encodeRedirectUrl(String s) {
+        return null;
+    }
+
+    @Override
+    public void sendError(int i, String s) throws IOException {
+
+    }
+
+    @Override
+    public void sendError(int i) throws IOException {
+
+    }
+
+    @Override
+    public void sendRedirect(String s) throws IOException {
+
+    }
+
+    @Override
+    public void setDateHeader(String s, long l) {
+
+    }
+
+    @Override
+    public void addDateHeader(String s, long l) {
+
+    }
+
+    @Override
+    public void setHeader(String s, String s1) {
+
+    }
+
+    @Override
+    public void addHeader(String s, String s1) {
+
+    }
+
+    @Override
+    public void setIntHeader(String s, int i) {
+
+    }
+
+    @Override
+    public void addIntHeader(String s, int i) {
+
+    }
+
+    @Override
+    public String getHeader(String s) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaders(String s) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
+        return null;
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return null;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) {
+
+    }
+
+    @Override
+    public void setContentLength(int i) {
+
+    }
+
+    @Override
+    public void setContentLengthLong(long l) {
+
+    }
+
+    @Override
+    public void setContentType(String s) {
+
+    }
+
+    @Override
+    public void setBufferSize(int i) {
+
+    }
+
+    @Override
+    public int getBufferSize() {
+        return 0;
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+
+    }
+
+    @Override
+    public void resetBuffer() {
+
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+}
---
+++ src/test/java/com/yahoo/bullet/rest/pubsub/PubSubControllerTest.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright 2016, Yahoo Inc.
+ *  Licensed under the terms of the Apache License, Version 2.0.
+ *  See the LICENSE file associated with the project for terms.
+ */
+package com.yahoo.bullet.rest.pubsub;
+
+import com.yahoo.bullet.rest.Mocks.MockHttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.CompletableFuture;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class PubSubControllerTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    PubSubController controller;
+
+    @Test
+    public void testPostAndGetQuery() throws Exception {
+        HttpServletResponse response = new MockHttpServletResponse();
+        controller.postQuery("{\"id\": \"88\", \"sequence\": -1, \"content\": \"foo\", \"metadata\": null}");
+        CompletableFuture<String> result = controller.getQuery(response);
+        Assert.assertEquals(response.getStatus(), 200);
+        Assert.assertEquals(result.get(), "{\"id\":\"88\",\"sequence\":-1,\"content\":\"foo\",\"metadata\":null}");
+        controller.getQuery(response);
+        Assert.assertEquals(response.getStatus(), 204);
+    }
+
+    @Test
+    public void testPostAndGetResult() throws Exception {
+        HttpServletResponse response = new MockHttpServletResponse();
+        controller.postResult("{\"id\": \"88\", \"sequence\": -1, \"content\": \"foo\", \"metadata\": null}");
+        CompletableFuture<String> result = controller.getResult(response);
+        Assert.assertEquals(response.getStatus(), 200);
+        Assert.assertEquals(result.get(), "{\"id\":\"88\",\"sequence\":-1,\"content\":\"foo\",\"metadata\":null}");
+        controller.getQuery(response);
+        Assert.assertEquals(response.getStatus(), 204);
+    }
+}
