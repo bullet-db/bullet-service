5850317d239f14a53ea83590013bb3821b6531ff
Haibin Xie <xie.haibin@yahoo.com>
2018-03-19 13:46:06 -0700

    Remove window information for http-query endpoint

--- src/main/java/com/yahoo/bullet/rest/controller/HTTPQueryController.java
+++ src/main/java/com/yahoo/bullet/rest/controller/HTTPQueryController.java
@@ -5,7 +5,15 @@
  */
 package com.yahoo.bullet.rest.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.yahoo.bullet.parsing.Clause;
+import com.yahoo.bullet.parsing.FieldTypeAdapterFactory;
+import com.yahoo.bullet.parsing.FilterClause;
+import com.yahoo.bullet.parsing.LogicalClause;
+import com.yahoo.bullet.parsing.Query;
 import com.yahoo.bullet.rest.query.HTTPQueryHandler;
+import com.yahoo.bullet.rest.query.QueryError;
 import com.yahoo.bullet.rest.query.SSEQueryHandler;
 import com.yahoo.bullet.rest.service.QueryService;
 import lombok.Setter;
@@ -23,18 +31,33 @@ public class HTTPQueryController {
     @Autowired @Setter
     private QueryService queryService;
 
+    private static final FieldTypeAdapterFactory<Clause> CLAUSE_FACTORY =
+            FieldTypeAdapterFactory.of(Clause.class, t -> t.getAsJsonObject().get(Clause.OPERATION_FIELD).getAsString())
+                    .registerSubType(FilterClause.class, Clause.Operation.RELATIONALS)
+                    .registerSubType(LogicalClause.class, Clause.Operation.LOGICALS);
+    private static final Gson GSON = new GsonBuilder().registerTypeAdapterFactory(CLAUSE_FACTORY)
+            .excludeFieldsWithoutExposeAnnotation()
+            .create();
+
     /**
      * The method that handles POSTs to this endpoint. Consumes the HTTP request, invokes {@link QueryService} to
      * register and transmit the query to Bullet.
      *
-     * @param query The JSON query.
+     * @param queryString The JSON query.
      * @return A {@link CompletableFuture} representing the eventual result.
      */
     @PostMapping(path = "${bullet.endpoint.http}", consumes = { MediaType.TEXT_PLAIN_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE })
-    public CompletableFuture<String> submitHTTPQuery(@RequestBody String query) {
+    public CompletableFuture<String> submitHTTPQuery(@RequestBody String queryString) {
         HTTPQueryHandler queryHandler = new HTTPQueryHandler();
         String queryID = QueryService.getNewQueryID();
-        queryService.submit(queryID, query, queryHandler);
+        // Remove window information from queryString since we don't support windowing for this endpoint.
+        try {
+            Query query = GSON.fromJson(queryString, Query.class);
+            query.setWindow(null);
+            queryService.submit(queryID, GSON.toJson(query), queryHandler);
+        } catch (Exception e) {
+            queryHandler.fail(QueryError.INVALID_QUERY);
+        }
         return queryHandler.getResult();
     }
 
